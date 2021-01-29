3a3066075cf502cd5a4338a0531066720e162fe4
Fckof <mr.pchelintsev@bk.ru>
2021-01-30 01:23:12 +0300

    hj

--- SNIIP/js/code.js
+++ SNIIP/js/code.js
@@ -1,9 +1,9 @@
-$(document).on('click', '.hex', function() {
-	// alert(this.attributes['value'].nodeValue);
-	// alert(this.attributes['value'].value);
-	// alert(this.getAttribute('value'));
+// $(document).on('click', '.hex', function() {
+// 	// alert(this.attributes['value'].nodeValue);
+// 	// alert(this.attributes['value'].value);
+// 	// alert(this.getAttribute('value'));
 	
-	var hex_id=this.getAttribute('value');
-	$(`#h${hex_id}`).toggleClass('blue_hex');
-	//alert(`#h${hex_id}`);
-});
+// 	var hex_id=this.getAttribute('value');
+// 	$(`#h${hex_id}`).toggleClass('blue_hex');
+// 	//alert(`#h${hex_id}`);
+// });
